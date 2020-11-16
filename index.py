2f8ec32af893353e482659214a6d619c9631484b
RobertDruska <robert.druska@gmail.com>
2020-11-16 15:03:00 +0100

    Revert "toto chcem revertnut"
    This reverts commit 4c3d7a71c4b50d01d694637602a53996d2e11030.

--- index.py
+++ index.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-base_url = 'https://www.chess.com/games/archive/el-camino12?show=live_blitz&gameType=live_blitz&rated=rated&page='
-number_of_pages = 3
+base_url = 'https://www.chess.com/games/archive/funo09?show=live_blitz&gameType=live_blitz&rated=rated&page='
+number_of_pages = 16
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -20,4 +20,4 @@ for i in range(1, number_of_pages + 1):
   download_button = driver.find_element_by_class_name("archive-games-download-button")
   download_button.click()
 
-  time.sleep(4)
+  time.sleep(2)
